7094dcb788857f5c42a4a395baa3e0272b3a2522
CarolinaOM <caro13081991@gmail.com>
2024-08-03 00:00:01 +0100

    objetos, definicion en linea, tipos perzonalizados

---
+++ BASES/objetos/objects.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    };
+    let superman = {
+        name: 'Clark KENT',
+        age: 60,
+        powers: ['Súper velocidad']
+    };
+})();
