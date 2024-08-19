70fca9b358ae191eaa09c99ce71aff39d98696ba
CarolinaOM <caro13081991@gmail.com>
2024-08-19 17:32:53 +0100

    forma corta de asignar propiedades

--- BASES/clases/basica.ts
+++ BASES/clases/basica.ts
@@ -1,21 +1,24 @@
 (() => {
 
     class Avenger {
-        private name: string;
-        public team: string;
-        private realName?: string;
+        //private name: string ;
+        //public team: string;
+        //private realName?: string;
         static avgAge: number = 35;
 
-        constructor(name: string, team: string, realName?: string){
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        }
+        constructor(
+            private name: string, 
+            private team: string, 
+            public realName?: string
+        ){}
     }
 
-    const antman: Avenger = new Avenger('Antman', 'Capitan');
+    const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     //console.log(antman);
     console.log(antman);
+
+    console.log(Avenger.avgAge);
+    
     
     
 })()
