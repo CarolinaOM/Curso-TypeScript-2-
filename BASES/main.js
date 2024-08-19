9c7663bd5f87ba41749ceb7c6061e87b402edc53
CarolinaOM <caro13081991@gmail.com>
2024-08-19 17:21:01 +0100

    clases basicas

--- BASES/main.js
+++ BASES/main.js
@@ -1,52 +1,14 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
         }
-    return t;
-};
-(() => {
-    const avengers = {
-        nick: 'Samuel L. Jackson',
-        iroman: 'Robert Downey Jr.',
-        vision: 'Paul Bettany',
-        activo: true,
-        poder: 1500
-    };
-    const printAvenger = (_a) => {
-        var { iroman } = _a, resto = __rest(_a, ["iroman"]);
-        console.log(iroman, resto);
-    };
-    const avengersArr = ['Cap. America', true, 150.15];
-    const [capitan, iroman, seriaUnNumero] = avengersArr;
-})();
-(() => {
-    const iroman = {
-        name: 'Iroman',
-        weapon: 'Armorsuit'
-    };
-    const capitanAmerica = {
-        name: 'Capitan America',
-        weapon: 'Escudo'
-    };
-    const thor = {
-        name: 'Thor',
-        weapon: 'Mjolnir'
-    };
-    const avengers = [iroman, thor, capitanAmerica];
-    for (const avenger of avengers) {
-        console.log(avenger.name, avenger.weapon);
     }
-})();
-(() => {
-    const nombre = 'Carolina';
-    const getName = () => {
-        console.log('Viejo getName');
-    };
+    Avenger.avgAge = 35;
+    const antman = new Avenger('Antman', 'Capitan');
+    console.log(antman);
 })();
 //# sourceMappingURL=main.js.map
