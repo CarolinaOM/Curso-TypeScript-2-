2597851743f7f1e6acfb3ef36dc4c0f49e791093
CarolinaOM <caro13081991@gmail.com>
2024-08-20 14:55:02 +0100

    metodos publicos y privados

--- BASES/clases/basica.ts
+++ BASES/clases/basica.ts
@@ -5,17 +5,30 @@
         //public team: string;
         //private realName?: string;
         static avgAge: number = 35;
+        static getAvgAge(){
+            return this.name;
+        }
 
         constructor(
             private name: string, 
             private team: string, 
             public realName?: string
         ){}
+
+        public bio(){
+            return `${this.name} (${this.team})`
+        }
     }
 
     const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     //console.log(antman);
     console.log(antman);
+
+    console.log(antman.bio());
+
+    console.log(Avenger.getAvgAge());
+    
+    
 
     console.log(Avenger.avgAge);
     
--- BASES/main.js
+++ BASES/main.js
@@ -1,15 +1,23 @@
 "use strict";
 (() => {
     class Avenger {
+        static getAvgAge() {
+            return this.name;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
     console.log(Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
