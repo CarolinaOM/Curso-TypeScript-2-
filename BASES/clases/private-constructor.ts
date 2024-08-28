5306521984be39e82409c458d3f05491e5b88448
CarolinaOM <caro13081991@gmail.com>
2024-08-28 16:31:17 +0100

    constructores privados

---
+++ BASES/clases/private-constructor.ts
@@ -0,0 +1,32 @@
+(()=> {
+    
+    class Apocalipsis {
+        static intance: Apocalipsis;
+        private  constructor(public name: string){
+        }
+        static callApocalipsis(): Apocalipsis{
+            if(!Apocalipsis.intance){
+                Apocalipsis.intance = new Apocalipsis('Soy apocalipsis el único');
+            }
+            return Apocalipsis.intance;
+        }
+
+        changeName(newName:string):void {
+            this.name = newName;
+        }
+    }
+
+    const apocalipsis1 = Apocalipsis.callApocalipsis()
+    const apocalipsis2 = Apocalipsis.callApocalipsis()
+    const apocalipsis3 = Apocalipsis.callApocalipsis()
+
+    apocalipsis1.changeName('Xavier');
+    //const apocalipsis1 = new Apocalipsis('Soy Apocalipsis1... el unico')
+    //const apocalipsis2 = new Apocalipsis('Soy Apocalipsis2... el unico')
+    //const apocalipsis3 = new Apocalipsis('Soy Apocalipsis3... el unico')
+
+    //console.log(apocalipsis1, apocalipsis2, apocalipsis3);
+    console.log(apocalipsis1, apocalipsis2, apocalipsis3);
+    
+    
+})()
