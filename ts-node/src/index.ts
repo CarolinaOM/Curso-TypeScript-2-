f5f7a586447e681a3f41260819ff2ab3dcb93efd
CarolinaOM <caro13081991@gmail.com>
2024-09-03 16:48:43 +0100

    Export defaults y exportacion con alias

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,5 +1,12 @@
-import { Hero } from "./clases/Hero";
+//import { Hero as SuperHero, Hero2 } from "./clases/Hero";
+import * as HeroClasses from "./clases/Hero";
+import  powers from "./data/powers";
 
-const iroman = new Hero('Iroman', 1, 56)
+const Hero = 123;
+
+//const iroman = new SuperHero('Iroman', 1, 56)
+const iroman = new HeroClasses.Hero('Iroman', 1, 56)
 
 console.log(iroman);
+
+console.log(powers);
