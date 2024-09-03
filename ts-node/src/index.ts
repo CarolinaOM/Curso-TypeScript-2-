b94c29296a736ee0703f025fbfe2114ec63f238c
CarolinaOM <caro13081991@gmail.com>
2024-09-03 17:03:21 +0100

    tarea

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,12 +1,10 @@
+import { Hero} from "./clases/Hero";
 //import { Hero as SuperHero, Hero2 } from "./clases/Hero";
-import * as HeroClasses from "./clases/Hero";
-import  powers from "./data/powers";
-
-const Hero = 123;
+//import * as HeroClasses from "./clases/Hero";
+import powers from './data/powers';
 
 //const iroman = new SuperHero('Iroman', 1, 56)
-const iroman = new HeroClasses.Hero('Iroman', 1, 56)
+const iroman = new Hero ('Iroman', 10, 56)
 
 console.log(iroman);
-
-console.log(powers);
+console.log(iroman.power);
