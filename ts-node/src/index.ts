a7dbda4f4b4a8133df54326c6231c714c36361ee
CarolinaOM <caro13081991@gmail.com>
2024-09-11 17:36:01 +0100

    quicktype.io extension

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,7 +1,7 @@
 import { getPokemon } from "./generics/get-pokemon";
 
 getPokemon(4)
-    .then(pokemon => console.log(pokemon.sprites.front_default))
+    .then(pokemon => console.log(pokemon))
     .catch(error => console.log(error))
     .finally(() => console.log('Fin de getPokemon'))
    
