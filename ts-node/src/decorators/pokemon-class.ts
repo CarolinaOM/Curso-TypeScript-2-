5de28e4386657d1a793eaa23c91dbc6db2a02062
CarolinaOM <caro13081991@gmail.com>
2024-09-12 14:23:41 +0100

    decoradores de fabrica

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -1,10 +1,16 @@
 function printToConsole (constructor: Function){
     console.log(constructor);
-    
 }
 
-@printToConsole
+const printToConsoleConditional = ( print: boolean = false):Function => {
+   if(print){
+    return printToConsole;
+   }else {
+    return () => {}
+   }
+}
 
+@printToConsoleConditional( false )
 export class Pokemon {
     public publicApi: string = 'https://pokeapi.co'
     constructor(
