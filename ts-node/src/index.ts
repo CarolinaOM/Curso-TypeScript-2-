39fa4303f9adb8dfb5affc3da98d735029544be9
CarolinaOM <caro13081991@gmail.com>
2024-09-16 15:50:15 +0100

    Decoradores de metodos

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -2,5 +2,5 @@ import { Pokemon } from "./decorators/pokemon-class"
 
 const charmader = new Pokemon ('Charmander');
 
-(Pokemon.prototype as any).customName = 'Pikachu'
-console.log(charmader);
+//(Pokemon.prototype as any).customName = 'Pikachu'
+console.log(charmader.savePokemonToDB(3));
